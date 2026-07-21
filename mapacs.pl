7c133c4fe6fc4c36749d56877c7e355aa805cb9d
4zv4l <46655455+4zv4l@users.noreply.github.com>
2026-07-21 13:48:06 +0800

    Add files via upload

---
+++ mapacs.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/env perl
+
+use Getopt::Long;
+use Pod::Usage;
+use File::Temp qw(tempfile);
+use File::Fetch;
+use strict;
+use warnings;
+
+# SIGINT Handler (Ctrl-c)
+#########################
+$SIG{INT} = sub {
+  print "\r[-] Ctrl-c received, exiting..\n";
+  exit 1;
+};
+
+$|++; # enable auto-flush
+
+# Argument parsing
+##################
+GetOptions(help => \my $help,
+           verbose => \my $verbose)
+  or pod2usage($!);
+pod2usage(0) if $help;
+
+# Scripts list
+##############
+my $scripts = [
+  ['WiFi Backend Switch' => 'https://raw.githubusercontent.com/O-APAC-SysAdmins/wifi_backend/refs/heads/main/wifi.sh'],
+  ['Freeze AMDGPU'       => 'https://raw.githubusercontent.com/O-APAC-SysAdmins/amd_freeze/refs/heads/main/freeze.sh'],
+  # add more here..
+];
+
+# Get script choice
+###################
+my $menu_idx = 0;
+my @menu     = map { sprintf "[%d] %s", ++$menu_idx, @$_[0] } @$scripts;
+
+my $user_choice;
+while (1) {
+  print join("\n", @menu), "\n\n> ";
+  $user_choice = <>;
+  last if $user_choice =~ /^\d+$/ and 1 <= $user_choice <= @$scripts;
+  printf "Please enter a number between 1 and %d..\n", scalar @$scripts;
+}
+my $choice = @$scripts[$user_choice-1];
+print "choice is: @$choice\n";
+
+# Download script
+#################
+my ($fh, $filename) = tempfile();
+print "[+] Temp file at: $filename\n" if $verbose;
+
+my $ff = File::Fetch->new(uri => @$choice[1]);
+$ff->fetch(to => \my $script_content) or die $ff->error;
+print "[+] Fetched @{[@$choice[1]]}\n" if $verbose;
+print $fh $script_content;
+
+# Execute script
+################
+chmod(0777, $filename);
+close $fh;
+exec $filename;
+
+__END__
+
+=head1 NAME
+
+mapacs - Master Asia Pacific Script
+
+=head1 SYNOPSIS
+
+mapacs.pl [options] [arguments]
+
+ Options:
+   -v, --verbose     Toggle verbose mode
+   -h, --help        Show this help
