a96236ada51b0df4e1351e0324df6d3d11884ae7
4zv4l <4zv4l@protonmail.com>
2026-07-29 12:38:35 +0800

    add: printerhk script

--- mapacs.pl
+++ mapacs.pl
@@ -29,6 +29,7 @@ pod2usage(0) if $help;
 my $scripts = [
   ['WiFi Backend Switch' => 'https://raw.githubusercontent.com/O-APAC-SysAdmins/wifi_backend/refs/heads/main/wifi.sh'],
   ['Freeze AMDGPU'       => 'https://raw.githubusercontent.com/O-APAC-SysAdmins/amd_freeze/refs/heads/main/freeze.sh'],
+  ['Printer HK'          => 'https://raw.githubusercontent.com/O-APAC-SysAdmins/printerHK/refs/heads/main/printerhk.pl'],
   # add more here..
 ];
 
